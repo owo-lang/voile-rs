5c53e8e9ff0c12efce0c7a46b98bdd1c4fac09f8
Yichen Yan <oraluben@outlook.com>
2019-04-11 21:15:00 +0800

    [ tyck ] Framework impl, check basic types, e.g. ``` val a : Type1; let a = Type0; ```

--- src/check/mod.rs
+++ src/check/mod.rs
@@ -1,4 +1,4 @@
-use crate::syntax::core::{GlobalEnv, Term};
+use crate::syntax::core::Term;
 use crate::syntax::env::GlobalEnv_;
 use crate::syntax::surf::ast::{Decl, Expr};
 use std::collections::BTreeMap;
@@ -6,9 +6,12 @@ use std::collections::BTreeMap;
 /// Type-Checking Error
 #[derive(Debug, PartialEq, Eq, Clone)]
 pub enum TCE {
-    SomeErr,
+    CouldNotInfer(Term),
+    NotImplemented,
 }
 
+/// env and gamma unit, todo: rename it
+#[derive(Debug, PartialEq, Eq, Clone)]
 pub struct S {
     term_value: Option<Term>,
     term_type: Option<Term>,
@@ -18,33 +21,77 @@ pub type TCM<T> = Result<T, TCE>;
 
 pub type TCS = GlobalEnv_<S>; // value and type
 
-pub fn check_main(_decls: Vec<Decl>) -> TCM<TCS> {
-    _decls.iter().fold(
+/// Expr -> (well-typed) Term
+pub fn check(tcs: TCS, expr: Expr) -> TCM<(TCS, Term)> {
+    match expr {
+        Expr::Type(_, level) => Ok((tcs, Term::Type(level))),
+        _ => Err(TCE::NotImplemented),
+    }
+}
+
+/// infer type of value
+pub fn check_infer(_tcs: TCS, value: Term) -> TCM<Term> {
+    use crate::syntax::core::Term::*;
+    match value {
+        Type(level) => Ok(Type(level + 1)),
+        _ => Err(TCE::CouldNotInfer(value)),
+    }
+}
+
+/// check if type1 is subtype of type2
+pub fn check_subtype(_tcs: TCS, subtype: Term, supertype: Term) -> TCM<Term> {
+    use crate::syntax::core::Term::*;
+    match (subtype, supertype) {
+        (Type(sub_level), Type(super_level)) if sub_level <= super_level => Ok(Type(super_level)),
+        _ => Err(TCE::NotImplemented),
+    }
+}
+
+pub fn check_main(decls: Vec<Decl>) -> TCM<TCS> {
+    decls.iter().fold(
         Result::Ok(BTreeMap::new()),
         |tcm: TCM<TCS>, decl| match tcm {
-            Ok(env) => check_decl(env, decl),
+            Ok(env) => check_decl(env, decl.clone()),
             _ => tcm,
         },
     )
 }
 
-pub fn check_decl(tcs: TCS, decl: &Decl) -> TCM<TCS> {
+pub fn check_decl(tcs: TCS, decl: Decl) -> TCM<TCS> {
     use crate::syntax::surf::ast::DeclKind::*;
-    let kind = decl.kind;
-    match kind {
-        Sign => check(tcs, &decl.body).map(|(tcs, val)| update_gamma(tcs, val)),
-        Impl => check(tcs, &decl.body).map(|(tcs, val)| update_env(tcs, val)),
+    let name = decl.name.clone();
+    match decl.kind {
+        Sign => {
+            let (tcs, val) = check(tcs, decl.body)?;
+            let mut tcs = tcs.clone();
+            tcs.insert(
+                name.info.text,
+                S {
+                    term_value: None,
+                    term_type: Some(val),
+                },
+            );
+            Ok(tcs)
+        }
+        Impl => {
+            let (tcs, val) = check(tcs, decl.body)?;
+            let ty = check_infer(tcs.clone(), val.clone())?;
+            let checked_ty = match tcs.get(&name.info.text).cloned() {
+                Some(S {
+                    term_type: Some(sig_ty),
+                    ..
+                }) => check_subtype(tcs.clone(), ty, sig_ty),
+                _ => Ok(ty),
+            }?;
+            let mut tcs = tcs.clone();
+            tcs.insert(
+                name.info.text,
+                S {
+                    term_value: Some(val),
+                    term_type: Some(checked_ty),
+                },
+            );
+            Ok(tcs)
+        }
     }
-}
-
-pub fn update_gamma(tcs: TCS, term: Term) -> TCS {
-    unimplemented!()
-}
-
-pub fn update_env(tcs: TCS, term: Term) -> TCS {
-    unimplemented!()
-}
-
-pub fn check(tcs: TCS, expr: &Expr) -> TCM<(TCS, Term)> {
-    unimplemented!()
 }
