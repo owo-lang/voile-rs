2751cc7b2affa3e12485adec6deb502fc809b907
ice1000 <ice1000kotlin@foxmail.com>
2019-04-20 04:20:19 -0400

    [ doc ] Write some sensational words

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,41 @@
 /*!
 Voile is a dependently-typed programming language evolved from [Mini-TT](https://docs.rs/minitt/).
 
+# Design
+
+## Goal
+
+The focus of Voile is *extensible algebraic data types*.
+It can solve the expression problem without using any design patterns (like visitor or
+object-algebra in Java, or finally-tagless or dtlac in Haskell).
+
+## Features
+
+Voile is supporting sum-types (coproduct), record types (product) as first-class
+language components.
+
+TODO Something need to be written here.
+
+# Implementation
+
+TODO Something need to be written here.
+
+## Problems
+
+TODO Something need to be written here.
+
+### About the name, Voile
+This name is inspired from a friend whose username is [*Voile*][vo] (or [*Voileexperiments*][ve]).
+However, this is also the name of a library in the [*Scarlet Devil Mansion*][sdm].
+
+<details>
+<summary>The librarian of Voile, the Magic Library</summary>
+![](https://raw.githubusercontent.com/owo-lang/voile-rs/master/rustdoc/voile-librarian.png)
+</details>
+
+ [vo]: https://www.codewars.com/users/Voile
+ [ve]: https://github.com/Voileexperiments
+ [sdm]: https://en.touhouwiki.net/wiki/Scarlet_Devil_Mansion
 */
 
 /// Abstract syntax, surface syntax, parser and well-typed terms (core language).
