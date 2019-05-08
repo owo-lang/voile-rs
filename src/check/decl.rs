eb5e33cef04b8b322c0340292bee8f4a2069df99
ice1000 <ice1000kotlin@foxmail.com>
2019-05-08 07:16:38 -0400

    [ decl ] Implement declaration checking

--- src/check/decl.rs
+++ src/check/decl.rs
@@ -2,6 +2,7 @@ use crate::syntax::abs::AbsDecl;
 
 use super::expr::{check, check_type};
 use super::monad::{TCM, TCS};
+use crate::syntax::core::Term;
 
 pub fn check_decls(mut tcs: TCS, decls: Vec<AbsDecl>) -> TCM {
     for decl in decls.into_iter() {
@@ -12,17 +13,33 @@ pub fn check_decls(mut tcs: TCS, decls: Vec<AbsDecl>) -> TCM {
 
 pub fn check_decl(tcs: TCS, decl: AbsDecl) -> TCM {
     match decl {
-        AbsDecl::Both(sign_info, sign_abs, _impl_info, impl_abs) => {
+        AbsDecl::Both(_, sign_abs, _, impl_abs) => {
             debug_assert_eq!(tcs.gamma.len(), tcs.env.len());
-            let (val, mut tcs) = check_type(tcs, sign_abs)?;
-            tcs.gamma.push(val.ast.clone().into_info(sign_info));
+            let (sign, tcs) = check_type(tcs, sign_abs)?;
+            let (val, mut tcs) = check(tcs, impl_abs, sign.ast.clone())?;
 
-            let (val, mut tcs) = check(tcs, impl_abs, val.ast)?;
+            tcs.gamma.push(sign);
             tcs.env.push(val);
             // Err(TCE::DbiOverflow(tcs.env.len(), new_dbi))
             Ok(tcs)
         }
-        _ => unreachable!(),
+        AbsDecl::Sign(_, sign_abs) => {
+            debug_assert_eq!(tcs.gamma.len(), tcs.env.len());
+            let syntax_info = sign_abs.syntax_info().clone();
+            let (val, mut tcs) = tcs.check_type(sign_abs)?;
+            tcs.env.push(Term::axiom().into_info(syntax_info));
+            tcs.gamma.push(val);
+            // Give warning on axiom?
+            Ok(tcs)
+        }
+        AbsDecl::Impl(_, impl_abs) => {
+            debug_assert_eq!(tcs.gamma.len(), tcs.env.len());
+            let (inferred, tcs) = tcs.infer(impl_abs.clone())?;
+            let (compiled, mut tcs) = tcs.unsafe_compile(impl_abs);
+            tcs.env.push(compiled);
+            tcs.gamma.push(inferred);
+            Ok(tcs)
+        }
     }
 }
 
