878cc1c8f3b0b658cdeeff96931ff93d1966222a
Yichen Yan <yichen.yan@inf.ethz.ch>
2019-05-10 11:50:10 +0200

    [ fix #52 ] shadowing `Lam`

--- src/syntax/abs/trans.rs
+++ src/syntax/abs/trans.rs
@@ -113,13 +113,16 @@ fn trans_expr_inner(
             let mut local_env = local_env.to_vec();
             let mut local_map = local_map.clone();
             for param in params {
-                // todo: add shadowing
-                assert!(!local_map.contains_key(&param.info.text));
-                local_env.insert(0, AbsDecl::None);
+                let shadowing = local_map.get(&param.info.text.clone()).cloned();
                 for (name, dbi) in local_map.iter_mut() {
+                    let dbi_value = *dbi;
                     *dbi += 1;
+                    if shadowing == Some(dbi_value) {
+                        local_env.remove(dbi_value);
+                    }
                 }
                 local_map.insert(param.info.text.clone(), 0);
+                local_env.insert(0, AbsDecl::None);
             }
             trans_expr_inner(body, env, global_map, &local_env, &local_map)
         }
