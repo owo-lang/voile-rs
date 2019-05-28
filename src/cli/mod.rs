6c96601423dd4d7462f8fcd947493d9bd7b74ef9
ice1000 <ice1000kotlin@foxmail.com>
2019-05-28 14:41:00 -0700

    [ #56 ] Start working on REPL

--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -14,22 +14,22 @@ fn main() {
         .file
         .clone()
         .and_then(|s| util::parse_file(s.as_str()))
-        .map(|ast| {
+        .map(|decls| {
             if !args.quiet {
                 println!("Parse successful.");
             }
 
             if !args.parse_only {
                 // Translate to abstract syntax
-                let abs = trans_decls(ast)
+                let abs_decls = trans_decls(decls)
                     .map_err(|err| eprintln!("{}", err))
                     .unwrap_or_else(|()| {
-                        eprintln!("Translate failed.");
+                        eprintln!("Scope-Check failed.");
                         std::process::exit(1)
                     });
 
                 // Type Check
-                let checked = check_main(abs)
+                let checked = check_main(abs_decls)
                     .map_err(|err| eprintln!("{}", err))
                     .unwrap_or_else(|()| {
                         eprintln!("Type-Check failed.");
--- src/cli/repl.rs
+++ src/cli/repl.rs
@@ -1,11 +1,250 @@
-#![allow(unused_variables, unused_mut)]
-
+use crate::util::parse_file;
+use rustyline::completion::{Completer, FilenameCompleter, Pair};
+use rustyline::error::ReadlineError;
+use rustyline::highlight::Highlighter;
+use rustyline::hint::Hinter;
+use rustyline::{CompletionType, Config, Context, Editor, Helper};
+use std::io::{stdin, stdout, Write};
+use voile::check::check_main;
 use voile::check::monad::TCS;
+use voile::syntax::abs::trans_decls;
+use voile::syntax::surf::{parse_str_err_printed, Decl};
+
+struct VoileHelper {
+    all_cmd: Vec<String>,
+    file_completer: FilenameCompleter,
+}
+
+impl Completer for VoileHelper {
+    type Candidate = Pair;
+
+    fn complete(
+        &self,
+        line: &str,
+        pos: usize,
+        ctx: &Context<'_>,
+    ) -> Result<(usize, Vec<Self::Candidate>), ReadlineError> {
+        if line.starts_with(LOAD_PFX) {
+            return self.file_completer.complete(line, pos, ctx);
+        }
+        Ok((
+            0,
+            self.all_cmd
+                .iter()
+                .filter(|cmd| cmd.starts_with(line))
+                .map(|str| Pair {
+                    display: str.clone(),
+                    replacement: str.clone(),
+                })
+                .collect(),
+        ))
+    }
+}
+
+impl Hinter for VoileHelper {
+    fn hint(&self, line: &str, pos: usize, _ctx: &Context<'_>) -> Option<String> {
+        if line.len() < 2 {
+            return None;
+        }
+        self.all_cmd
+            .iter()
+            .filter(|cmd| cmd.starts_with(line))
+            .cloned()
+            .map(|cmd| cmd[pos..].to_string())
+            .next()
+    }
+}
+
+impl Highlighter for VoileHelper {}
+
+impl Helper for VoileHelper {}
+
+const PROMPT: &'static str = "=> ";
+const QUIT_CMD: &'static str = ":quit";
+const GAMMA_CMD: &'static str = ":gamma";
+const CTX_CMD: &'static str = ":context";
+const HELP_CMD: &'static str = ":help";
+const LOAD_CMD: &'static str = ":load";
+const TYPE_CMD: &'static str = ":type";
+const INFER_CMD: &'static str = ":infer";
+const LEXICAL_CMD: &'static str = ":lexical";
+
+/// Used for REPL command
+const LOAD_PFX: &'static str = ":load ";
+/// Used for REPL command
+const TYPE_PFX: &'static str = ":type ";
+const INFER_PFX: &'static str = ":infer ";
+const LEVEL_PFX: &'static str = ":level ";
+const LEXICAL_PFX: &'static str = ":lexical ";
+
+fn show_gamma(tcs: &TCS) {
+    for val in tcs.gamma {
+        println!("val {};", val.ast);
+    }
+}
+
+fn show_telescope(tcs: &TCS) {
+    for val in tcs.env {
+        println!("let {};", val.ast);
+    }
+}
+
+fn repl_work(tcs: TCS, current_mode: &str, line: &str) -> Option<TCS> {
+    if line == QUIT_CMD {
+        None
+    } else if line.is_empty() {
+        Some(tcs)
+    } else if line == GAMMA_CMD {
+        show_gamma(&tcs);
+        Some(tcs)
+    } else if line == CTX_CMD {
+        show_telescope(&tcs);
+        Some(tcs)
+    } else if line == HELP_CMD {
+        help(current_mode);
+        Some(tcs)
+    } else if line.starts_with(LOAD_PFX) {
+        Some(
+            match parse_file(line.trim_start_matches(LOAD_CMD).trim_start()) {
+                Some(decls) => update_tcs(tcs, decls),
+                None => tcs,
+            },
+        )
+    } else if line.starts_with(INFER_PFX) {
+        let line = line.trim_start_matches(INFER_CMD).trim_start();
+        unimplemented!();
+        Some(tcs)
+    } else if line.starts_with(':') {
+        println!("Unrecognized command: {}", line);
+        println!("Maybe you want to get some `:help`?");
+        Some(tcs)
+    } else {
+        Some(match parse_str_err_printed(line).ok() {
+            Some(decls) => update_tcs(tcs, decls),
+            None => tcs,
+        })
+    }
+}
+
+fn update_tcs(tcs: TCS, decls: Vec<Decl>) -> TCS {
+    let abs_decls = trans_decls(decls)
+        .map_err(|err| eprintln!("{}", err))
+        .unwrap_or_default();
+    check_main(abs_decls)
+        .map_err(|err| eprintln!("{}", err))
+        .unwrap_or_default()
+}
+
+fn help(current_mode: &str) {
+    repl_welcome_message(current_mode);
+    println!(
+        "\
+         Commands:\n\
+         {:<20} {}\n\
+         {:<20} {}\n\
+         {:<20} {}\n\
+         {:<20} {}\n\
+         {:<20} {}\n\
+         {:<20} {}\n\
+         ",
+        QUIT_CMD,
+        "Quit the REPL.",
+        GAMMA_CMD,
+        "Show current typing context.",
+        CTX_CMD,
+        "Show current value context.",
+        ":lexical <EXPR>",
+        "Show the lexical information of the expression.",
+        ":load <FILE>",
+        "Load an external file.",
+        ":infer <EXPR>",
+        "Try to infer the type of the given expression.",
+    );
+}
+
+fn repl_welcome_message(current_mode: &str) {
+    println!(
+        "Interactive voilec {}\n\
+         Source code: https://github.com/owo-lang/voile-rs\n\
+         Issue tracker: https://github.com/owo-lang/voile-rs/issues/new\n\n\
+
+         The REPL has two modes: the RICH mode and the PLAIN mode.\n\
+         Completion, history command, hints and (in the future) colored output are available in the \
+         rich mode, but does not work entirely under Windows PowerShell ISE and Mintty \
+         (Cygwin, MinGW and (possibly, depends on your installation) git-bash).\n\
+         You are using the {} mode.\n\
+         ",
+        env!("CARGO_PKG_VERSION"),
+        current_mode
+    );
+}
 
 pub fn repl_plain(mut tcs: TCS) {
-    unimplemented!()
+    repl_welcome_message("PLAIN");
+    let stdin = stdin();
+    loop {
+        print!("{}", PROMPT);
+        stdout().flush().expect("Cannot flush stdout!");
+        let mut line = String::new();
+        stdin.read_line(&mut line).expect("Cannot flush stdout!");
+        if let Some(ok) = repl_work(tcs, "PLAIN", line.trim()) {
+            tcs = ok;
+        } else {
+            break;
+        };
+    }
 }
 
 pub fn repl(mut tcs: TCS) {
-    unimplemented!()
+    repl_welcome_message("RICH");
+    let all_cmd: Vec<_> = vec![
+        QUIT_CMD,
+        GAMMA_CMD,
+        CTX_CMD,
+        HELP_CMD,
+        LOAD_CMD,
+        TYPE_CMD,
+        INFER_CMD,
+        LEXICAL_CMD,
+    ]
+    .iter()
+    .map(|s| s.to_string())
+    .collect();
+    let mut r = Editor::with_config(
+        Config::builder()
+            .history_ignore_space(true)
+            .completion_type(CompletionType::Circular)
+            .build(),
+    );
+    r.set_helper(Some(VoileHelper {
+        all_cmd,
+        file_completer: FilenameCompleter::new(),
+    }));
+    // Load history?
+    loop {
+        match r.readline(PROMPT) {
+            Ok(line) => {
+                let line = line.trim();
+                r.add_history_entry(line.as_ref());
+                if let Some(ok) = repl_work(tcs, "RICH", line) {
+                    tcs = ok;
+                } else {
+                    break;
+                };
+            }
+            Err(ReadlineError::Interrupted) => {
+                println!("Interrupted by Ctrl-c.");
+                break;
+            }
+            Err(ReadlineError::Eof) => {
+                println!("Interrupted by Ctrl-d");
+                break;
+            }
+            Err(err) => {
+                println!("Error: {:?}", err);
+                break;
+            }
+        };
+    }
+    // Write history?
 }
