a0ea912cb6d3708ef9e1b7c0694f8b785e33e9ad
ice1000 <ice1000kotlin@foxmail.com>
2019-09-21 16:56:16 -0400

    [ util ] Depend on the subcrate

--- voile-util/src/lib.rs
+++ voile-util/src/lib.rs
@@ -40,7 +40,7 @@ pub mod pest_util;
 #[cfg(feature = "lisp")]
 pub mod lisp;
 
-/// Not using https://docs.rs/vec1/1.4.0 because it doesn't have `fold1`.
+/// Not using the [vec1 crate](https://docs.rs/vec1/1.4.0) because it doesn't have `fold1`.
 pub mod vec1;
 
 /// Generic meta variable utilities.
